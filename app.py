8b92bf499816303a9b239220295dd0e686487822
Alvix.exe <alvix.wick@gmail.com>
2025-06-15 15:19:51 -0400

    Stored the API response in Redis; also added a check to see if the requested city is in the cache to avoid unnecessary API calls.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query
 from dotenv import load_dotenv
 import os
+import redis.client
 import uvicorn
 import httpx
 import redis
@@ -15,13 +16,22 @@ redis_client = redis.Redis(host='localhost', port=6379, db=0)
 @app.get("/weather")
 async def get_weather(city: str = Query(..., description="Name city")):
     url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?key={API_KEY}"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        data = response.json()
-
-        result = {
-                "city": city,
-                "temperature": data["currentConditions"]["temp"],
-                "description": data["currentConditions"]["conditions"]
-                }
+    cached = redis_client.get(city)
+    if cached:
+        result = json.loads(cached)
+        print("Devuelto de redis")
         return result
+    else:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            data = response.json()
+
+            result = {
+                    "city": city,
+                    "temperature": data["currentConditions"]["temp"],
+                    "description": data["currentConditions"]["conditions"]
+                    }
+
+            redis_client.set(city, json.dumps(result))
+            print("devuelto de la api")
+            return result
