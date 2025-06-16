6840caa58def742e835930b2e0340fc7c2bfc4df
Alvix.exe <alvix.wick@gmail.com>
2025-06-16 10:09:53 -0400

    Added validation in case there is an API error or the city does not exist.

--- app.py
+++ app.py
@@ -15,23 +15,35 @@ redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.get("/weather")
 async def get_weather(city: str = Query(..., description="Name city")):
+
     url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?key={API_KEY}"
     cached = redis_client.get(city)
+
     if cached:
         result = json.loads(cached)
         print("Devuelto de redis")
         return result
+    
     else:
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
-            data = response.json()
 
-            result = {
-                    "city": city,
-                    "temperature": data["currentConditions"]["temp"],
-                    "description": data["currentConditions"]["conditions"]
-                    }
+            if response.status_code != 200:
+                return {"Error": "City not found or API error."}
+            
+            else:
+                data = response.json()
 
-            redis_client.set(city, json.dumps(result), ex=3600)
-            print("devuelto de la api")
-            return result
+                result = {
+                        "city": city,
+                        "temperature": data["currentConditions"]["temp"],
+                        "description": data["currentConditions"]["conditions"]
+                        }
+
+                redis_client.set(city, json.dumps(result), ex=3600)
+                print("devuelto de la api")
+                return result
+
+for key in redis_client.keys():
+    value = redis_client.get(key)
+    print(key.decode(), value.decode())
