3fcfad8f265e044bbc73b470b73db66f64e99d15
Alvix.exe <alvix.wick@gmail.com>
2025-06-30 10:35:13 -0400

    Received a complete update due to modularization and continues to work perfectly.

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI, Query, HTTPException
 from dotenv import load_dotenv
 import os
-import uvicorn
-import httpx
-import redis
 import json
 import logging
+from cache.redis_cache import redis_client
+from services.weather_service import fetch_weather
 
 logging.basicConfig(
     level=logging.INFO,
@@ -16,15 +15,15 @@ app = FastAPI()
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.get("/weather")
 async def get_weather(city: str = Query(..., description="Name city")):
-
+    """
+    Returns the current weather and the 15-day forecast for the specified city.
+    Uses Redis for caching and handles errors from the external API.
+    """
     if not city or not city.strip():
         raise HTTPException(status_code=400, detail="City parameter cannot be empty.")
-
-    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?key={API_KEY}"
 
     city_key = city.strip().lower()
     cached = redis_client.get(city_key)
@@ -33,69 +32,9 @@ async def get_weather(city: str = Query(..., description="Name city")):
         result = json.loads(cached)
         logging.info("Information obtained from Redis")
         return result
-    
-    else:
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.get(url)
-                logging.info("Requesting the API correctly")
-        except httpx.RequestError:
-            logging.error("An error occurred while connecting to the API.")
-            raise HTTPException(status_code=503, detail="Could not connect to the weather service.")
-        
-        response_detail = handle_errors(response)
-        if response_detail:
-            logging.error(f"Unexpected response code {response.status_code}")
-            raise HTTPException(status_code=response.status_code, detail=response_detail)
-        
-        else:
-            data = response.json()
-            if "currentConditions" not in data:
-                logging.critical("The current conditions were not returned by the API.")
-                raise HTTPException(status_code=500, detail="Unexpected response from the weather API.")
-            
-            temperature = fahrenheit_to_celsius(data["currentConditions"]["temp"])
-            result = {
-                    "Current conditions":{
-                                        "city": data["resolvedAddress"],
-                                        "temperature": f"{temperature}",
-                                        "humidity": data["currentConditions"]["humidity"],
-                                        "description": data["currentConditions"]["conditions"],
-                                        },
-                    "For the next 15 days":{    
-                                            "description": data["description"],   
-                                            }
-                    }
-            redis_client.set(city_key, json.dumps(result), ex=3600)
-            logging.info("Information obtained from API")
-            return result
 
-'''for key in redis_client.keys():
-    value = redis_client.get(key)
-    print(key.decode(), value.decode())'''
-
-def fahrenheit_to_celsius(fahrenheit: float):
-    try:
-        celsius = round((fahrenheit - 32) * 5 / 9, 2)
-        logging.info(f"Converted {fahrenheit}F to {celsius}C")
-        return celsius
-    except Exception as e:
-        logging.info(f"Unexpected error in converting Fahrenheit to Celsius: {e}")
-        return fahrenheit
-
-def handle_errors(response):
-    if response.status_code == 404:
-        return "Page not found"
-    elif response.status_code == 500:
-        return "A general error occurred while processing the request."
-    elif response.status_code == 400:
-        return "The format of the API is incorrect or an invalid parameter or combination of parameters was supplied."
-    elif response.status_code == 401:
-        return"There is a problem with the API key, account or subscription."
-    elif response.status_code == 429:
-        return "The account has exceeded their assigned limits."
-    elif response.status_code != 200:
-        return f"Unexpected error: {response.status_code}"
-    return None
-        
-#redis_client.flushdb()
+    # If not in cache, queries the external API.
+    result = await fetch_weather(city, API_KEY)
+    redis_client.set(city_key, json.dumps(result), ex=3600)
+    logging.info("Information obtained from API and cached")
+    return result
